b9ea2d2ee16e3c8e03a9e858e23f11157bd392be
Jeriming <316906528@qq.com>
2023-02-04 00:34:56 +0800

    修复通过umd打包后,使用nodeJs运行，报错 self is not defined

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, "lib"),
     filename: "hash32.js",
     library: "hash32",
+    // 通过umd打包后,使用nodeJs运行，报错 self is not defined
+    // 发现打包后没有：typeof self !== 'undefined' ? self : this
+    // 直接使用了 self传入，查看官方文档发现需要设置globalObject为this
     libraryTarget: "umd",
+    globalObject: 'this',
     clean: true,
   },
   module: {
